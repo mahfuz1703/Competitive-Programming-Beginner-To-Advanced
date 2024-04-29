9b555082f94fd7284508c3aeb6408c341ce66534
Mahfuz <sayhello.mahfuz@gmail.com>
2024-04-29 14:59:39 +0600

    add bit manipulation operations

--- Bit Manipulation/some_bit_operations.cpp
+++ Bit Manipulation/some_bit_operations.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 #define ll long long
 
+// check kTH bit on/off
 int check_kTH_Bit(int n, int k){
     return (n >> k) & 1;
 }
 
+// print all on  bits of a number
 void print_on_bits(int n){
     for(int k = 0; k < 32; k++){
         if(check_kTH_Bit(n, k)){
@@ -15,21 +17,24 @@ void print_on_bits(int n){
     cout << "\n";
 }
 
+// check a number even or odd
 bool even_or_odd(int n){
     if(n & 1) return 0;
     else return 1;
 }
 
-void count_on_bits(int n){
+// count on bits of a number
+int count_on_bits(int n){
     int cnt = 0;
     for(int k = 0; k < 32; k++){
         if(check_kTH_Bit(n, k)){
             cnt++;
         }
     }
-    cout << cnt << "\n";
+    return cnt;
 }
 
+// print all off bits of a number
 void print_off_bits(int n){
     for(int k = 0; k < 32; k++){
         if(!check_kTH_Bit(n, k)){
@@ -38,12 +43,50 @@ void print_off_bits(int n){
     }
     cout << "\n";
 }
+
+// set kTH bit on
+int set_kTH_bit_on(int n, int k){
+    return (1 << k) | n;
+}
+
+// set kTH bit off
+int set_kTH_bit_off(int n,  int k){
+    return (~(1 << k)) & n;
+}
+
+// kTH bit toggle of a number
+int toggle_kTH_bit(int x, int k){
+    return x ^ (1 << k);
+}
+
+int multiply_by_2(int n){
+    return n << 1;
+}
+
+int divide_by_2(int n){
+    return n >> 1;
+}
+
+int power_of_2(int n){
+    return 1 << n;
+}
+
+bool check_power_of_2(int n){
+    return (count_on_bits(n) == 1);
+}
 int main(){
     
-    int n; cin >> n;
-    // print_on_bits(n);
+    // int n; cin >> n;
+    // print_on_bits(5);
     // print_off_bits(n);
     // count_on_bits(n);
-    cout << even_or_odd(n) << "\n";
+    // cout << even_or_odd(n) << "\n";
+    // cout << set_kTH_bit_on(11, 2) << "\n";
+    // cout << set_kTH_bit_off(15, 2) << "\n";
+    // cout << toggle_kTH_bit(11, 2) << "\n";
+    // cout << multiply_by_2(100) << "\n";
+    // cout << divide_by_2(100) << "\n";
+    // cout << power_of_2(4) << "\n";
+    cout << check_power_of_2(16) << "\n";
     return 0;
 }
