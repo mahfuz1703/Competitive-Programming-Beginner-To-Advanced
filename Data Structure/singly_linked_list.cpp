7917f76960ad34ffc1f49ea718a9141e5cb98258
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2025-01-05 02:04:36 +0600

    add singly linked list and a_power_b using DP

---
+++ Data Structure/singly_linked_list.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+	int value;
+	Node *next;
+
+	Node(int value){
+		this->value = value;
+		this->next = NULL;
+	}
+};
+
+void insert_at_head(Node* &head, int value){
+	Node *newnode = new Node(value);
+	newnode->next = head;
+	head = newnode;
+}
+
+void insert_at_tail(Node* &head, int value){
+	Node *newnode = new Node(value);
+
+	if(head == NULL) {
+		head = newnode;
+		return;
+	}
+
+	Node *tmp = head;
+	while(tmp->next != NULL){
+		tmp = tmp->next;
+	}
+	tmp->next = newnode;
+}
+
+void insert_at_any_pos(Node* &head, int indx, int value){
+	Node *newnode = new Node(value);
+	Node *tmp = head;
+	for(int i = 1; i < indx; i++){
+		tmp = tmp->next;
+	}
+	newnode->next = tmp->next;
+	tmp->next = newnode;
+}
+
+void print(Node *head){
+	Node *tmp = head;
+	while(tmp != NULL){
+		cout << tmp->value << "\n";
+		tmp = tmp->next;
+	}
+}
+
+int main(){
+	// Node a(10), b(20), c(30);
+
+	// a.value = 10;
+	// b.value = 20;
+	// c.value = 30;
+
+	// a.next = &b;
+	// b.next = &c;
+	// c.next = NULL;
+
+
+	// dynamic node create
+	Node *head = new Node(10);
+	Node *a = new Node(20);
+	Node *b = new Node(30);
+	Node *c = new Node(40);
+
+	head->next = a;
+	a->next = b;
+	b->next = c;
+
+	// cout << head->value << "\n";
+	// cout << head->next->value << "\n";
+	// cout << head->next->next->value << "\n";
+
+	// print all values uisng loop
+	// print(head); // print linkedList
+	// cout << "\n";
+
+
+	// insert new node at heat
+	// insert_at_head(head, 50);
+	// insert_at_head(head, 60);
+
+
+	// Node* neww = NULL;
+	// // insert new node at tail
+	// insert_at_tail(neww, 50);
+	// insert_at_tail(neww, 60);
+	// print(neww); // print linkedList
+
+
+	
+	insert_at_any_pos(head, 2, 25);
+	insert_at_any_pos(head, 4, 35);
+	print(head); // print linkedList
+}
---
+++ Dynamic Programming/a_power_b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int power(int a, int b){
+    if(b == 0) return 1;
+
+    if(b % 2 == 0){
+        int res = power(a, b/2);
+        return res * res;
+    }else{
+        return power(a, b-1) * 2;
+    }
+}
+int main(){
+    int a, b; cin >> a >> b;
+
+    cout << power(a, b) << "\n";
+    return 0;
+}
